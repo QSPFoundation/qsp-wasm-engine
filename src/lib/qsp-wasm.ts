5496cbd397d1c53927fda3907f71cda883b2231b
Sergii Kostyrko <skostyrko@waverleysoftware.com>
2022-07-13 09:47:44 +0300

    update init to accept wasm content

--- src/lib/qsp-wasm.ts
+++ src/lib/qsp-wasm.ts
@@ -1,25 +1,13 @@
 import { QspAPI } from '../contracts/api';
-import qsp from '../qsplib/public/qsp-wasm';
-import qspModule from '../qsplib/public/qsp-wasm.wasm';
+import createQspModule from '../qsplib/public/qsp-wasm';
 
 import { QspAPIImpl } from './qsp-api';
 
-// Since webpack will change the name and potentially the path of the
-// `.wasm` file, we have to provide a `locateFile()` hook to redirect
-// to the appropriate URL.
-// More details: https://kripken.github.io/emscripten-site/docs/api_reference/module.html
-const moduleWasmPromise = qsp({
-  locateFile(path: string) {
-    if (path.endsWith('.wasm')) {
-      return qspModule;
-    }
-    return path;
-  },
-});
-
-export function init(): Promise<QspAPI> {
+export function init(wasm: ArrayBufferView | ArrayBuffer): Promise<QspAPI> {
   return new Promise((resolve) => {
-    moduleWasmPromise.then((moduleWasm) => {
+    createQspModule({
+      wasm,
+    }).then((moduleWasm) => {
       resolve(new QspAPIImpl(moduleWasm));
     });
   });
