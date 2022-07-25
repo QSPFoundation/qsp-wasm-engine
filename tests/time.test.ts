95ba90f1277ecc44a041fb1695880d0d0780e73e
Sergii Kostyrko <skostyrko@waverleysoftware.com>
2022-07-25 15:28:54 +0300

    test time

---
+++ tests/time.test.ts
@@ -0,0 +1,85 @@
+import { prepareApi, runTestFile } from '../src/test-helpers';
+import { jest } from '@jest/globals';
+import { Mock } from 'jest-mock';
+import { QspAPI } from '../src/contracts/api';
+
+describe('time', () => {
+  let api: QspAPI;
+  let error: Mock;
+  beforeEach(async () => {
+    api = await prepareApi();
+    error = jest.fn();
+    api.on('error', error);
+  });
+
+  test('WAIT should trigger wait event', () => {
+    const onWait = jest.fn();
+    api.on('wait', onWait);
+    runTestFile(api, `WAIT 1000`);
+    expect(error).not.toHaveBeenCalled();
+    expect(onWait).toHaveBeenCalledWith(1000, expect.any(Function));
+  });
+
+  test('WAIT should pause execution', () => {
+    const onWait = jest.fn();
+    api.on('wait', onWait);
+    const onStatsChanged = jest.fn();
+    api.on('stats_changed', onStatsChanged);
+    runTestFile(api, `WAIT 1000 & p 'after wait'`);
+    expect(onStatsChanged).toHaveBeenCalledWith('');
+    expect(onWait).toHaveBeenCalledWith(1000, expect.any(Function));
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    (onWait.mock.calls[0][1] as Function)();
+    expect(onStatsChanged).toHaveBeenCalledWith('after wait');
+  });
+
+  test.skip('MSECSCOUNT should return msec passed from game start', () => {
+    // TODO find out way to test this
+  });
+
+  test('SETTIMER should change counter frequency', () => {
+    const onTimer = jest.fn();
+    api.on('timer', onTimer);
+    runTestFile(api, `SETTIMER 100`);
+    expect(error).not.toHaveBeenCalled();
+    expect(onTimer).toHaveBeenCalledWith(100);
+  });
+
+  test('counter location should be called', () => {
+    const onStatsChanged = jest.fn();
+    api.on('stats_changed', onStatsChanged);
+    runTestFile(
+      api,
+      `
+$counter = 'counter'
+---
+#counter
+p 'works'
+`
+    );
+    expect(error).not.toHaveBeenCalled();
+    api.execCounter();
+    expect(onStatsChanged).toBeCalledWith('works');
+  });
+
+  test('several counters should be supported', () => {
+    const onStatsChanged = jest.fn();
+    api.on('stats_changed', onStatsChanged);
+    runTestFile(
+      api,
+      `
+$counter = 'counter'
+$counter[] = 'counter_other'
+---
+#counter
+p 'works'
+-- 
+#counter_other
+p ' & works other'
+`
+    );
+    expect(error).not.toHaveBeenCalled();
+    api.execCounter();
+    expect(onStatsChanged).toBeCalledWith('works & works other');
+  });
+});
