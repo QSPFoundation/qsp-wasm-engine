d2ef792c315a3ded570e4ffb30aec8ff31aa3bb7
Sergii Kostyrko <skostyrko@waverleysoftware.com>
2022-08-15 18:45:33 +0300

    test multi assignment

--- tests/variables.test.ts
+++ tests/variables.test.ts
@@ -22,21 +22,47 @@ describe('stats panel', () => {
 
   test('variables are case insensitive', () => {
     runTestFile(api, `money = 100 & MONEY += 1`);
+    expect(error).not.toHaveBeenCalled();
     expect(api.readVariable('MoNeY')).toBe(101);
   });
 
   test('LET assignment', () => {
     runTestFile(api, `LET money = 100`);
+    expect(error).not.toHaveBeenCalled();
     expect(api.readVariable('money')).toBe(100);
   });
 
   test('SET assignment', () => {
     runTestFile(api, `SET money = 100`);
+    expect(error).not.toHaveBeenCalled();
     expect(api.readVariable('money')).toBe(100);
   });
 
   test('assignment', () => {
     runTestFile(api, `money = 100`);
+    expect(error).not.toHaveBeenCalled();
     expect(api.readVariable('money')).toBe(100);
+  });
+
+  test('multi assignmet', () => {
+    runTestFile(api, `a, $b, $c, d = 1, 'test', 'other', 2`);
+    expect(error).not.toHaveBeenCalled();
+    expect(api.readVariable('a')).toBe(1);
+    expect(api.readVariable('$b')).toBe('test');
+    expect(api.readVariable('$c')).toBe('other');
+    expect(api.readVariable('d')).toBe(2);
+  });
+
+  test('variable swap', () => {
+    runTestFile(
+      api,
+      `
+x = 1
+y = 2
+y, x  = x, y`
+    );
+    expect(error).not.toHaveBeenCalled();
+    expect(api.readVariable('x')).toBe(2);
+    expect(api.readVariable('y')).toBe(1);
   });
 });
