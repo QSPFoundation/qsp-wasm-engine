dcb54f6214a45c849b48f38a8c3e534cc77649ef
Sergii Kostyrko <skostyrko@waverleysoftware.com>
2022-09-12 15:12:40 +0300

    rework build

--- src/qsp-engine-browser.ts
+++
@@ -1,7 +0,0 @@
-import { QspAPI } from './contracts/api';
-import { init } from './lib/qsp-engine';
-
-export async function initQspEngine(wasmPath: string): Promise<QspAPI> {
-  const wasm = await fetch(wasmPath).then((r) => r.arrayBuffer());
-  return init(wasm);
-}
--- src/qsp-engine-node.ts
+++
@@ -1,8 +0,0 @@
-import { QspAPI } from './contracts/api';
-import { init } from './lib/qsp-engine';
-import fsp from 'fs/promises';
-
-export async function initQspEngine(filePath: string): Promise<QspAPI> {
-  const wasm = await fsp.readFile(filePath);
-  return init(wasm.buffer);
-}
